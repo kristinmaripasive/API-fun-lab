3f6c12476b3bb9b0b415668ef673cca760d4c3d6
Kristin Pasive <kristin.pasive@gmail.com>
2016-06-16 15:41:53 -0400

    basic functionality and css added

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
+
   $("button").click(function(){
     var value = $("input").val();
-    var url = "http://accesscontrolalloworiginall.herokuapp.com/https://api.spotify.com/v1/tracks/1zHlj4dQ8ZAtrayhuDDmkY?market=ES"
+    var url = "https://www.omdbapi.com/?t="+value
 
     $.ajax({
       url: url,
@@ -10,6 +11,23 @@ $(document).ready(function(){
     })
     .done(function(response){
       console.log(response);
+
+  //refers to the objects in the API and places them in each made variable
+  var picture = "<img class='picture' src = '"+ response.Poster +"'>";
+  var title = "<h1>"+ response.Title + "</h1>";
+  var rate = "<p>"+ response.Rated + "</p>";
+  var year = "<p>"+ response.Year + "</p>";
+  var plot = "<p>"+ response.Plot + "</p>";
+
+//append picture to html
+  $(picture).appendTo($(".picture"));
+
+//append responses to html
+  $(title).appendTo($(".content"));
+  $(rate).appendTo($(".content"));
+  $(year).appendTo($(".content"));
+  $(plot).appendTo($(".content"));
+
     })
     .fail(function(response){
       console.log("failedddd",response);
@@ -17,5 +35,6 @@ $(document).ready(function(){
     .always(function(response){
       console.log("DOING SOMETHING");
     })
+
   });
 });
